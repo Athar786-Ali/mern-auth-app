cdf38f1f2e6dfc48966cc8e13cf28d57524ff141
Md Athar Ali <mdatharsbr@gmail.com>
2026-01-14 19:51:34 +0530

    Fix Render port issue

--- server/server.js
+++ server/server.js
@@ -1,20 +1,17 @@
-import express, { json } from "express"
-import cors from "cors"
-import 'dotenv/config'
-import cookieParser from "cookie-parser"
-import connectDB from "./config/mongodb.js"
-import authRouter from "./routes/authRoutes.js"
-import userRouter from "./routes/userRoutes.js"
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
-const app=express()
+const app = express();
 app.set("trust proxy", 1);
 
-const port = process.env.PORT || 4000
-
-connectDB();
-
-app.use(express.json())
-app.use(cookieParser())
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
   origin: [
     "http://localhost:3000",
@@ -23,15 +20,15 @@ app.use(cors({
   credentials: true
 }));
 
+// Connect DB
+connectDB();
 
+app.get("/", (req, res) => {
+  res.send("API is working fine...🙂");
+});
 
-//Api endpoints
-app.get('/',(req,res)=>{
-    res.send("API is working fine....... okay")
-})
-app.use('/api/auth',authRouter)
-app.use('/api/user',userRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
-
-app.listen(port,()=> console.log(`Server is running on port :${port}`))
-
+const port = process.env.PORT || 10000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
